2ce2893c1584ebcc5e4dd0849d05bc11930912ac
mhaava <mhaava@tlu.ee>
2019-10-01 20:05:46 +0300

    uus stiil

--- main/views.py
+++ main/views.py
@@ -4,12 +4,14 @@ from main.models import Tootaja
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/index.html')
+    args = {'page_url': request.get_full_path()}
+    return render(request, 'main/index.html', args)
     
 
 def vaatatootaja(request):
     tootajad = Tootaja.objects.all()
-    args = {'tootajad': tootajad}
+    args = {'tootajad': tootajad,
+            'page_url': request.get_full_path()}
     return render(request, 'main/vaatatootaja.html', args)
     
 
@@ -21,15 +23,10 @@ def lisatootaja(request):
       asutus = Asutus.objects.only('id').get(pk=asutus)
       Tootaja.objects.create(asutus=asutus, eesNimi=eesNimi, perekonnaNimi=perekonnaNimi)
     
-    args = {'asutused' : Asutus.objects.all()}
+    args = {'asutused' : Asutus.objects.all(),
+            'page_url': request.get_full_path()}
     return render(request, 'main/lisatootaja.html', args)
 
-def lisaasutus(request):
-    if request.method == "POST":
-      nimi = request.POST.get('nimi', '')
-      Asutus.objects.create(nimi=nimi)
-    
-    return render(request, 'main/lisaasutus.html')
 
 def kustutatootaja(request):
   tootaja_id = request.GET.get('id', '')
@@ -37,3 +34,10 @@ def kustutatootaja(request):
   tootaja.delete()
 
   return HttpResponseRedirect('/vaatatootaja')
+
+def lisaasutus(request):
+    if request.method == "POST":
+      nimi = request.POST.get('nimi', '')
+      Asutus.objects.create(nimi=nimi)
+    args = {'page_url': request.get_full_path()}
+    return render(request, 'main/lisaasutus.html', args)
