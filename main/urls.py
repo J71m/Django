17962037c48298e4dbfdeb13948c5eb8d767ae31
Tim Jaanson <timj@tlu.ee>
2019-10-28 12:31:30 +0200

    added features

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
   url(r'^vaatatootaja/', views.vaatatootaja, name='vaatatootaja'),
   url(r'^lisatootaja/', views.lisatootaja, name='lisatootaja'),
   url(r'^kustutatootaja/', views.kustutatootaja, name='kustutatootaja'),
-  url(r'^lisaasutus/', views.lisaasutus, name='lisaasutus')
+  url(r'^lisaasutus/', views.lisaasutus, name='lisaasutus'),
+  url(r'^vaataasutus/', views.vaataasutus, name='vaataasutus'),
+  url(r'^muudaasutusnimi/', views.muudaasutusnimi, name='muudaasutusnimi'),
+  url(r'^muudaasutus/', views.muudaasutus, name='muudaasutus'),
+  url(r'^kustutaasutus/', views.kustutaasutus, name='kustutaasutus')
 ]
--- main/views.py
+++ main/views.py
@@ -35,9 +35,47 @@ def kustutatootaja(request):
 
   return HttpResponseRedirect('/vaatatootaja')
 
+def vaataasutus(request, linkedworker=False):
+  asutused = Asutus.objects.all()
+  args = {'asutused': asutused,
+          'linkedworker': linkedworker,
+          'page_url': request.get_full_path()}
+  return render(request, 'main/vaataasutus.html', args)
+
 def lisaasutus(request):
     if request.method == "POST":
       nimi = request.POST.get('nimi', '')
       Asutus.objects.create(nimi=nimi)
     args = {'page_url': request.get_full_path()}
     return render(request, 'main/lisaasutus.html', args)
+
+def muudaasutus(request):
+  asutus = Asutus.objects.get(id=request.GET.get('id', ''))
+  args = {'asutus': asutus,
+          'page_url': request.get_full_path()}
+  return render(request, 'main/muudaasutus.html', args)
+
+def muudaasutusnimi(request):
+    if request.method == "POST":
+      asutus_id = request.POST.get('id', '')
+      asutus_uus = request.POST.get('nimi', '')
+      asutus = Asutus.objects.get(id=asutus_id)
+      asutus.nimi = asutus_uus
+      asutus.save()
+    #args = {'page_url': request.get_full_path()}
+    return HttpResponseRedirect('/vaataasutus')
+
+def kustutaasutus(request):
+  asutus_id = request.GET.get('id', '')
+  asutus = Asutus.objects.get(id=asutus_id)
+  #tootajad = 0
+  try:
+    tootajad = Tootaja.objects.filter(asutus=asutus)
+  except Tootaja.DoesNotExist:
+    pass
+
+  if not tootajad:
+    asutus.delete()
+    return vaataasutus(request, False)
+  else:
+    return vaataasutus(request, True)
